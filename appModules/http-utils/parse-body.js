2de551875767b410c6e6a7d15534b7ce2ebd19e2
Xerobrin120 <151674035+Xerobrin120@users.noreply.github.com>
2024-05-12 16:27:01 +0300

    Revert "исправил ошибки."

--- appModules/http-utils/parse-body.js
+++ appModules/http-utils/parse-body.js
@@ -5,7 +5,7 @@ let body = "";
       body += chunk.toString();
     });
     req.on("end", () => {
-      resolve(body);
+      resolve(null, body);
     });
     req.on("error", (error) => {
         reject(error, null);
